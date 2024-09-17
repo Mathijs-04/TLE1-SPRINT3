de322712031acd65efe3d0283c7e055b4a66a4ec
J-Hoogvliet <justinhoogvliet@outlook.com>
2024-09-17 11:52:35 +0200

    Informatie op tonen op experience pagina<- werkt nog niet

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 window.addEventListener('load', init);
 
 function init() {
+    AJAXRequest("data.php", mainSecMaker);
+}
+function AJAXRequest(location, successCallback) {
+    fetch(location)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(successCallback)
+        .catch(errorHandler);
+    console.log("Information fetched")
+}
 
-    fillDivs();
-    updateFeaturedProjects();
+function mainSecMaker(data){
+for (let experience of data){
+    const expDiv = document.querySelectorAll("#different")
+    const p = document.querySelector(".context");
+    p.innerText = experience.experience;
+    expDiv.append(p);
+}
+}
+function errorHandler(){
+    console.log('Geen informatie opgehaald')
 }
