b210beb0d051f442b923e1b78659224400458fdf
J-Hoogvliet <justinhoogvliet@outlook.com>
2024-09-17 15:27:14 +0200

    js voor experience

--- includes/actions.php
+++ includes/actions.php
@@ -34,7 +34,7 @@ function experienceDetails($id): array
     $tags = [
         1 => [
             "experience" => "Boswandeling: Geniet van de natuur",
-            "expImage" => "./img/forest_walk_foto.webp",
+            "expImage" => "./img/placeholder.png",
             "information" => "Ervaar de rust van een boswandeling in onze Boswandeling Experience. Wandel over een zacht pad omringd door hoge bomen en groene bladeren. Terwijl je loopt, hoor je het zachte gezang van vogels en voel je de frisse lucht op je huid. De zon schijnt door de takken, wat een prettige sfeer creëert. Dit is een ideale manier om even uit te rusten en simpelweg te genieten van de natuurlijke schoonheid om je heen. Het is de meest gewone ervaring, maar eentje die je de kans geeft om te ontspannen en op te laden.",
             "Expurl" => "",
         ],
