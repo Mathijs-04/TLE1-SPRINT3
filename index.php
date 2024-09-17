1f29327d3c5ffc1578f53ebf71cf7caf8d4c462a
Mathijs-04 <mathijsvandermeijde@gmail.com>
2024-09-17 14:29:40 +0200

    Header Fixed

--- index.php
+++ index.php
@@ -10,14 +10,9 @@
     <title>Home</title>
 </head>
 <body>
-<nav>
-    <a href="index.php" class="gobackindex"><img src="./img/go-back-2.png" alt="goback"></a >
-    <a href="index.php" class="tohome"><img src="./img/expcorp.webp" alt="logo" class="logo"></a>
-    <a href="index.php" class="gobackindex"><img src="./img/go-back-2.png" alt="goback"></a >
-
-</nav>
 <header>
-
+    <img src="./img/expcorpstacked.webp" alt="logo" class="highResLogo">
+    <img src="./img/Header.webp" alt="logo" class="homeLogo">
 </header>
 <main>
     <div>
