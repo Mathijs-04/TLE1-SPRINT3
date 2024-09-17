452cb9ccb15cf50940fa190811eea4bbbec1cc9c
wesle <wesleynederpel43@gmail.com>
2024-09-17 14:38:20 +0200

    Walk in the forest info

--- includes/actions.php
+++ includes/actions.php
@@ -6,23 +6,25 @@
 function experienceListMaker(): array
 {
     return [
-    [
-        "id" => 1,
-        "experience" => "groene draak",
-        "expImage" => ""
-    ],
-    [
-        "id" => 2,
-        "experience" => "vier",
-        "expImage" => ""
-    ],[
-        "id" => 3,
-        "experience" => "vijf",
-        "expImage" => ""
-    ],
+        [
+            "id" => 1,
+            "experience" => "groene draak",
+            "expImage" => ""
+        ],
+        [
+            "id" => 2,
+            "experience" => "vier",
+            "expImage" => ""
+        ], [
+            "id" => 3,
+            "experience" => "vijf",
+            "expImage" => ""
+        ],
     ];
 
-} /**
+}
+
+/**
  * @param $id
  * @return array
  */
@@ -31,9 +33,9 @@ function experienceDetails($id): array
 {
     $tags = [
         1 => [
-            "experience" => "gele voeten",
-            "expImage" => "",
-            "information" => "fietsen met voeten",
+            "experience" => "Boswandeling: Geniet van de natuur",
+            "expImage" => "./img/forest_walk_foto.webp",
+            "information" => "Ervaar de rust van een boswandeling in onze Boswandeling Experience. Wandel over een zacht pad omringd door hoge bomen en groene bladeren. Terwijl je loopt, hoor je het zachte gezang van vogels en voel je de frisse lucht op je huid. De zon schijnt door de takken, wat een prettige sfeer creëert. Dit is een ideale manier om even uit te rusten en simpelweg te genieten van de natuurlijke schoonheid om je heen. Het is de meest gewone ervaring, maar eentje die je de kans geeft om te ontspannen en op te laden.",
             "Expurl" => "",
         ],
         2 => [
