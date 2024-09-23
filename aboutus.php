ab1f0ee51ddaa4923278bcca5cb3076e7035e05a
Geert van der Velden <1079252@hr.nl>
2024-09-23 14:44:02 +0200

    new pages

---
+++ aboutus.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./css/style.css" rel="stylesheet">
+    <script src="./js/main.js"></script>
+    <title>Review</title>
+</head>
+<body class="indexBody">
+
+<nav>
+    <img src="./img/expcorpstacked.webp" alt="logo" class="highResLogo">
+    <div class="navlinkera">
+        <a href="index.php" class="navlinks">
+            <div>home</div>
+        </a>
+        <a href="experiences.php" class="navlinks">
+            <div>ervaring</div>
+        </a>
+        <a href="aboutus.php" class="navlinks">
+            <div>over ons</div>
+        </a>
+        <a href="Review.php" class="navlinks">
+            <div>Review</div>
+        </a>
+
+
+    </div>
+
+</nav>
+<header>
+
+</header>
+<main>
+
+</main>
+<footer>
+    <p>©EXPCORP 2073</p>
+</footer>
+</body>
+</html>
