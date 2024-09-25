7fed5140908f13fa51625b901c03165b1cb5772b
abigail-Bridget <abigailbridget@outlook.com>
2024-09-25 14:40:09 +0200

    ik ben bezig met de expierences

--- webservice/includes/actions.php
+++ webservice/includes/actions.php
@@ -35,24 +35,36 @@ function getDishDetails($id)
            "Expurl" => "",
         ],
         "Entertaiment" => [
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
+            "experience" => "Ontdek de Kunstzalen van Statens Museum",
+            "Image" => "./img/Museum_360.webp",
+            "information" => "Stap in de wereld van kunst en cultuur met deze unieke VR-ervaring! Verken Sal 217A van het Statens Museum for Kunst in Kopenhagen, en laat je omarmen door de schoonheid van de kunstwerken die deze ruimte sieren. Met een 360-graden fotografie kun je je onderdompelen in de indrukwekkende zalen van het museum, alsof je er zelf bent. Kijk om je heen, bewonder de details van de schilderijen, en voel de sfeer van de bijzondere tentoonstellingen. Of je nu een kunstliefhebber bent of gewoon nieuwsgierig, deze ervaring biedt je de kans om kunst van dichtbij te bekijken. Laat je inspireren door de prachtige ruimte en de rijke geschiedenis van het museum, allemaal vanuit het comfort van je eigen huis. Zet je VR-bril op en laat je meevoeren in de wonderlijke wereld van Sal 217A.",
+            "Expurl" => "",
         ],[
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
+            "experience" => "Verken de Pracht van het Bellagio",
+            "Image" => "./img/Bellagio_360.webp",
+            "information" => "Stap binnen in de adembenemende lobby van het Bellagio Hotel in Las Vegas, waar kunst en luxe samenkomen. Met deze VR-ervaring kun je het beroemde Chihuly plafond bewonderen, dat bestaat uit duizenden kleurrijke glasbladeren die als een levendig kunstwerk boven je hoofd hangen. Voel je alsof je echt in de lobby bent terwijl je om je heen kijkt en de schitterende details van het plafond in je opneemt. De sprankelende kleuren en unieke vormen van het glas maken deze ruimte tot een onvergetelijke bezienswaardigheid. Deze ervaring is perfect voor iedereen die nieuwsgierig is naar de schoonheid van het Bellagio en de artistieke creativiteit van Dale Chihuly. Geniet van de magie van Las Vegas en laat je betoveren door dit unieke stukje kunst, allemaal vanuit het comfort van je eigen huis. Zet je VR-bril op en laat je meevoeren in de prachtige wereld van het Bellagio.",
+            "Expurl" => "",
         ],[
-            "recipe" => "You can make this delicious Dutch meal by ...",
-            "tags" => ['unox', 'healthy', 'stamppot', 'boerenkool']
+            "experience" => "Verken Inside Pionir",
+            "Image" => "./img/Belgrado_360.webp",
+            "information" => "Stap binnen in het hart van de sportcultuur van Belgrado met deze unieke VR-ervaring. Verken Inside Pionir, de iconische thuisbasis van Basketball Club Partizan, waar geschiedenis en passie samenkomen. Voel de opwinding van de sport terwijl je rondkijkt in deze indrukwekkende arena. Zie de tribunes gevuld met enthousiaste fans en de energie die door de lucht stroomt tijdens een spannende wedstrijd. Met elk detail dat je ontdekt, krijg je een gevoel van de rijke traditie en de trots die de club met zich meebrengt. Of je nu een fan bent van basketball of gewoon nieuwsgierig naar de sfeer in de arena, deze ervaring laat je de intensiteit van een livewedstrijd beleven, allemaal vanuit het comfort van je eigen huis. Zet je VR-bril op en laat je meeslepen in de wereld van Partizan.",
+            "Expurl" => "",
         ],
         "Reizen" => [
-            "recipe" => "Very nice when your grandma prepares this meal",
-            "tags" => ['omnomnom']
+            "experience" => "Een Virtuele Reis door een Dynamische Haven",
+            "Image" => "./img/Harbor_360.webp",
+            "information" => "Stap aan boord voor een unieke virtuele reis door Harbor City, een bruisende haven vol leven en activiteit. Met deze VR-ervaring kun je de prachtige haven en zijn omgeving verkennen, van de indrukwekkende schepen tot de levendige sfeer van de stad.Kijk om je heen en ontdek de verschillende facetten van het havenleven. Voel de energie van de stad terwijl je langs de kades wandelt, het geluid van zeewater en het geroezemoes van mensen om je heen. Deze ervaring biedt een kijkje in de dagelijkse activiteiten en de schoonheid van Harbor City.Of je nu meer wilt leren over de haven, de scheepvaartgeschiedenis wilt ontdekken, of gewoon wilt genieten van de uitzichten, deze ervaring brengt de dynamiek van Harbor City naar je toe, allemaal vanuit het comfort van je eigen huis. Zet je VR-bril op en laat je meevoeren naar deze levendige bestemming.",
+            "Expurl" => "",
         ],[
-            "recipe" => "Very nice when your grandma prepares this meal",
-            "tags" => ['omnomnom']
+            "experience" => "Ontdek Parijs",
+            "Image" => "./img/Paris_360.webp",
+            "information" => "Stap binnen in de betoverende wereld van Parijs met deze unieke immersieve VR-ervaring. Verken de iconische bezienswaardigheden, van de majestueuze Eiffeltoren tot de charmante straatjes van Montmartre, en laat je onderdompelen in de rijke cultuur en geschiedenis van de stad.Met een interactieve kijkervaring kun je rondkijken en genieten van de adembenemende uitzichten die Parijs te bieden heeft. Hoor het zachte geruis van de stad, het gelach van mensen in de cafés, en de klanken van straatmuzikanten die de levendige sfeer van Parijs vastleggen. Of je nu een ervaren reiziger bent of de stad voor het eerst ontdekt, deze ervaring brengt de magie van Parijs naar je toe. Geniet van deze virtuele tour en laat je inspireren door de schoonheid en de charme van de Franse hoofdstad, allemaal vanuit het comfort van je eigen huis. Zet je VR-bril op en ontdek de wonderen van Parijs.",
+            "Expurl" => "",
         ],[
-            "recipe" => "Very nice when your grandma prepares this meal",
-            "tags" => ['omnomnom']
+            "experience" => "Ontdek de Schoonheid van een Traditionele Japanse Tuin",
+            "Image" => "./img/Japanese_360.webp",
+            "information" => "Stap binnen in een serene wereld van schoonheid en harmonie met deze unieke VR-ervaring in de traditionele Japanse tuin van het Sumida-ku Taisho Fork House Museum. Deze prachtige tuin biedt een rustige ontsnapping aan de drukte van het dagelijks leven en laat je kennismaken met de rijke cultuur en esthetiek van Japan. Met de interactieve viewer kun je rondkijken en de verfijnde details van de tuin bewonderen, van de zorgvuldig aangelegde paden en de prachtig vormgegeven planten tot de traditionele architectuur die het landschap siert. Voel de kalmte om je heen en luister naar het rustgevende geluid van kabbelend water en fluitende vogels. Deze ervaring is perfect voor iedereen die geïnteresseerd is in Japanse cultuur of gewoon even wil ontspannen in een prachtige omgeving. Geniet van de schoonheid en de sereniteit van de traditionele Japanse tuin, allemaal vanuit het comfort van je eigen huis. Zet je VR-bril op en laat je meevoeren naar deze betoverende plek.",
+            "Expurl" => "",
         ],
 
     ];
