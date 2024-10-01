5a8ef0ac02c026b4c4c2fceafbb700dce05b2628
wesle <wesleynederpel43@gmail.com>
2024-10-01 14:10:02 +0200

    Helaas en Gelukkig php files

---
+++ helaas.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="./css/style.css" rel="stylesheet">
+    <script src="./js/main.js"></script>
+    <title>Helaas</title>
+</head>
+<body>
+<nav>
+    <a href="index.php">
+        <img src="./img/expcorp.webp" alt="logo" class="navlogo">
+    </a>
+    <div class="navlinkera">
+        <a href="index.php" class="navlinks">Home</a>
+        <a href="experiences.php" class="navlinks">Ervaring</a>
+        <a href="aboutus.php" class="navlinks">Over Ons</a>
+        <a href="product.php" class="navlinks">Product</a>
+    </div>
+
+</nav>
+<main>
+    <div class="product">
+
+        <div class="productInfo">
+            <h1 class="productName">Helaas...</h1>
+            <p>Beste klant,<br>
+                Als u gevoelig bent voor bewegingsziekten tijdens het gebruik van VR raden wij het af om onze ervaringen
+                te gebruiken.<br>
+                Wij bieden geen headsets aan voor mensen die gevoelig zijn voor deze bewegingsziekten.</p>
+        </div>
+</main>
+<footer>
+    <div>
+        <img class="footerImg" src="./img/expcorpwhite.webp" alt="footerLogo">
+        <div class="copyright">
+            <p>© EXPCORP 2024</p>
+        </div>
+    </div>
+    <div class="footerContent">
+        <div class="legals">
+            <a href="https://drive.google.com/file/d/1Dgpov7vXRdX2jVjCYHre-El7Ds2jDf0H/view" target="_blank">Privacyverklaring</a>
+            <a href="https://drive.google.com/file/d/1Dgpov7vXRdX2jVjCYHre-El7Ds2jDf0H/view" target="_blank">Algemene
+                voorwaarden</a>
+            <a href="https://drive.google.com/file/d/1Dgpov7vXRdX2jVjCYHre-El7Ds2jDf0H/view" target="_blank">Cookiebeleid</a>
+            <a href="contact.php">Contact</a>
+        </div>
+    </div>
+</footer>
+</body>
+</html>
